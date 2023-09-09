fbe23c58b30ae5be8f54f2c6cebeb324dc28dc74
felipe gomez <ggfelipegc@gmail.com>
2023-09-09 09:27:19 -0500

    feat: add LanguageData type

---
+++ src/types/textarea.d.ts
@@ -0,0 +1,9 @@
+import { UnitedKingdomFlag } from "@/assets/icons";
+
+export type LanguageData = {
+  locale: string;
+  description: string;
+  value: string;
+  placeholder: string;
+  flag: typeof UnitedKingdomFlag;
+};
