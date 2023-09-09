4728079feaa8cadb05c3121dcd4be80ee9adc285
felipe gomez <ggfelipegc@gmail.com>
2023-09-09 09:19:17 -0500

    build: add styles to context and @ shortcut

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,50 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { fileURLToPath, URL } from "url";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+        @import "src/styles/_normalize.scss";
+        @import "src/styles/_variables.scss";
+        `,
+      },
+    },
+  },
+  resolve: {
+    alias: [
+      {
+        find: "@",
+        replacement: fileURLToPath(new URL("./src", import.meta.url)),
+      },
+      {
+        find: "@assets",
+        replacement: fileURLToPath(
+          new URL("./src/shared/assets", import.meta.url)
+        ),
+      },
+      {
+        find: "@cmp",
+        replacement: fileURLToPath(
+          new URL("./src/shared/cmp", import.meta.url)
+        ),
+      },
+      {
+        find: "@stores",
+        replacement: fileURLToPath(
+          new URL("./src/shared/stores", import.meta.url)
+        ),
+      },
+      {
+        find: "@use",
+        replacement: fileURLToPath(
+          new URL("./src/shared/use", import.meta.url)
+        ),
+      },
+    ],
+  },
+});
