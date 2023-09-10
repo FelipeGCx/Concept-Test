3bc6d8635edfe2e8b3609f18f6697ed294920af2
felipe gomez <ggfelipegc@gmail.com>
2023-09-10 11:17:29 -0500

    feat: implement update emits

--- src/app.vue
+++ src/app.vue
@@ -1,11 +1,19 @@
 <template>
   <Information />
-  <TextAreaMultilanguage />
+  <TextAreaMultilanguage
+    @update="updateReceiver"
+    @updateList="updateReceiver"
+  />
 </template>
 
 <script setup lang="ts">
 import TextAreaMultilanguage from "@/components/TextAreaMultilanguage.vue";
 import Information from "@/components/Information.vue";
+import { MinLanguageData } from "@/types";
+
+const updateReceiver = (value: string | Array<MinLanguageData>) => {
+  console.info({ value });
+};
 </script>
 
 <style scoped></style>
