9f10ab9bdacd18a4669e77b6e8b275f6f2b672e3
felipe gomez <ggfelipegc@gmail.com>
2023-09-10 11:12:15 -0500

    feat: implement emits

--- src/components/TextAreaMultilanguage.vue
+++ src/components/TextAreaMultilanguage.vue
@@ -1,12 +1,18 @@
 <script setup lang="ts">
 import { markRaw, ref } from "vue";
-import { LanguageData } from "@/types";
+import { LanguageData, MinLanguageData } from "@/types";
 import {
   UnitedKingdomFlag,
   ColombiaFlag,
   ItalyFlag,
   FranceFlag,
 } from "@/assets/icons";
+
+interface Emits {
+  (e: "update", value: string): void;
+  (e: "updateList", value: Array<MinLanguageData>): void;
+}
+const emit = defineEmits<Emits>();
 
 const languagesValues = ref<Array<LanguageData>>([
   {
@@ -48,6 +54,14 @@ const changeLanguage = () => {
       ? languagesValues.value[0]
       : languagesValues.value[index + 1];
 };
+const updatedValue = () => {
+  emit("update", currentItem.value.value);
+  const languageItem = languagesValues.value.map((item: LanguageData) => {
+    const { locale, value } = item;
+    return { locale, value };
+  });
+  emit("updateList", languageItem);
+};
 </script>
 <template>
   <div class="note-container">
@@ -61,6 +75,7 @@ const changeLanguage = () => {
       cols="30"
       rows="10"
       v-model="currentItem.value"
+      @change="updatedValue"
       :placeholder="currentItem.placeholder"
     ></textarea>
   </div>
